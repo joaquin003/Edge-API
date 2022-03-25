0472dd9f1fac750b03d7b6c554f631bd2f545d06
Joaquin Uliambre <joaquinuliambre.ju@gmail.com>
2022-03-25 13:02:04 -0300

    Agregamos pagina de inicio y de error 404

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const axios = require("axios");
 const app = express();
 const PORT = 3001;
 
+app.set("json spaces", 2);
+
+app.get("/", (req, res) => {
+  res.send("Bienvenidos a la app!");
+});
+
+//si no existe la ruta, mostramos mensaje de error
+app.use((req, res) => {
+  if (res) {
+    res.status(404).send({ error: "Pagina no encontrada" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log("Corriendo servidor");
 });
